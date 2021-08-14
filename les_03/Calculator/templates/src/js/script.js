97006f04db421f9a23633d66203b05115445650b
Nguyễn Đức Quân <71173459+ducquanbn@users.noreply.github.com>
2021-08-14 12:46:35 +0700

    Update script.js

--- les_03/Calculator/templates/src/js/script.js
+++ les_03/Calculator/templates/src/js/script.js
@@ -1,15 +1,32 @@
-
-const display = document.getElementById("display");
-
-document.getElementById("del").onclick = function(){
-    display.value ="0"; 
+let display = document.getElementById("display");
+document.getElementById("del").onclick = function () {
+    display.value = "0";
 }
 
-cal =(val) =>{
-    if(display.value == "0"){
+cal = (val) => {
+    if (display.value == "0") {
         display.value = val;
-    }else{
+    } else {
         display.value += val;
     }
 }
 
+
+document.getElementById("calcul").addEventListener("click", () => {
+    let dataReq=JSON.stringify({"exprestion": display.value})
+    fetch('http://localhost:8080/cal', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: dataReq,
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("display").value = data.exprestion;
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+    });
+})
