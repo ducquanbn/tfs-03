62c31abf492108e4519fc0a4c20dc710faef2035
Nguyễn Đức Quân <71173459+ducquanbn@users.noreply.github.com>
2021-08-14 12:45:56 +0700

    Update main.go

--- les_03/Calculator/main.go
+++ les_03/Calculator/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type Exps struct {
+	expression string
+}
+
+func cal(w http.ResponseWriter, r *http.Request) {
+	var exps Exps
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&exps)
+	if err != nil {
+		http.Error(w, "BAD REQUEST", http.StatusBadRequest)
+		return
+	}
+	encoder := json.NewEncoder(w)
+	fmt.Println(exps.expression + "asdas") //Khong ra Data cua Res
+	result := Exps{expression: "123"}
+	encoder.Encode(result)
+
+}
 
 var templates *template.Template
 
@@ -13,6 +34,7 @@ func main() {
 	templates = template.Must(template.ParseGlob("templates/*html"))
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandle).Methods("GET")
+	r.HandleFunc("/cal", cal).Methods("POST")
 	fs := http.FileServer(http.Dir("./templates/src"))
 	r.PathPrefix("/src/").Handler(http.StripPrefix("/src", fs))
 	http.Handle("/", r)
